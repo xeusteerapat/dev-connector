79df8a047ee2f206f493e795a7c950c74b0e7fc3
xeusteerapat <xeus085@gmail.com>
2020-04-11 00:05:30 +0700

    add dashboard for authorized user

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user">Welcome {user && user.name}</i>
+      </p>
+    </>
+  );
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
