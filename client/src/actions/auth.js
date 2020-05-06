d5dce747c501bf73ccfb2af4cc1cadd908eba756
xeusteerapat <xeus085@gmail.com>
2020-05-06 20:06:46 +0700

    update individual user profile page

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -92,10 +92,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / clear profile
 export const logout = () => dispatch => {
-  dispatch({
-    type: CLEAR_PROFILE
-  });
-  dispatch({
-    type: LOGOUT
-  });
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
 };
